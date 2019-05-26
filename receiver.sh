55a531c3ac8b33a25f4c3ad1ecc9aa9b64e9b510
jinbinhu <45911482+jinbinhu@users.noreply.github.com>
2019-05-26 18:00:56 +0800

    Add files via upload

---
+++ receiver.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Please enter ETH name: e.g. eth0" 
+read ETH
+echo "Please enter DST_IP address: e.g. 192.168.188.133"
+read DSTIP
+echo "Please enter SRC_IP address: e.g. 192.168.188.136" 
+read SRCIP
+echo "Please enter SRCMAC address: e.g. 0x00,0x0C,0x29,0x7E,0xB1,0x35" 
+read SMAC
+echo "Please enter DSTMAC address: e.g. 0x00,0x0C,0x29,0x7E,0xB1,0x35" 
+read DMAC
+
+
+eth="#define ETH \"$ETH\""
+dstip="#define DST_IP \"$DSTIP\""
+srcip="#define SRC_IP \"$SRCIP\""
+smac="unsigned char SMAC[ETH_ALEN] = {$SMAC};"
+dmac="unsigned char DMAC[ETH_ALEN] = {$DMAC};"
+
+
+cd receiver 
+sed -i "/#define ETH/c\\$eth" decodemod.c
+sed -i "/#define DST_IP/c\\$dstip" decodemod.c
+sed -i "/#define SRC_IP/c\\$srcip" decodemod.c
+sed -i "/unsigned char SMAC[ETH_ALEN]/c\\$smac" decodemod.c
+sed -i "/unsigned char DMAC[ETH_ALEN]/c\\$smac" decodemod.c
+
+sudo make
+
+sudo insmod decodemod.ko
+
+
